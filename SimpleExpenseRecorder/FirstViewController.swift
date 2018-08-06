43d4562f0de24ee0ac5022c07e5452f5716c3601
khashi001 <taurus@kokoroyutakani.com>
2018-08-06 14:37:45 +0900

    Initial Commit

---
+++ SimpleExpenseRecorder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SimpleExpenseRecorder
+//
+//  Created by 橋本久美子 on 2018/08/06.
+//  Copyright © 2018年 Orange Forest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
